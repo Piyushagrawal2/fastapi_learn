f6cbb53968817468d4ec2e3434cf7bc45583fde0
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2025-06-12 21:31:35 +0530

    feat: implement database session management for blog creation endpoint

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models, database
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=database.engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post("/blog")
-def create(request: schemas.Blog):
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    db.add(models.Blog(**request.dict()))
+    db.commit()
+    db.refresh()
     return request
